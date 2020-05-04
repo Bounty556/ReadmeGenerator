cc3c00a2bd35798cdd3fa65e7d0ee66d8833bd69
Bounty556 <jacobmayday@gmail.com>
2020-05-04 17:41:26 -0600

    Installed packages, wrote questions

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
 
+const questions = [
+    'What is your github username?',
+    'What is the name or URL of the repository you wish to generate a README for?'
 ];
 
 function writeToFile(fileName, data) {
+    
 }
-
-function init() {
-
-}
-
-init();
